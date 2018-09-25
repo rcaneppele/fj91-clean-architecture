c2aade6008aa347eb7ea2d89737123b58e897eb7
Rodrigo da Silva Ferreira Caneppele <r.caneppele@gmail.com>
2018-09-25 11:39:12 -0300

    update senha mysql

--- rh-persistencia/src/main/java/br/com/caelum/fj91/rh/persistencia/JpaConfiguration.java
+++ rh-persistencia/src/main/java/br/com/caelum/fj91/rh/persistencia/JpaConfiguration.java
@@ -27,7 +27,7 @@ public class JpaConfiguration {
 		dataSource.setDriverClassName("com.mysql.jdbc.Driver");
 		dataSource.setUrl("jdbc:mysql://localhost:3306/rh?createDatabaseIfNotExist=true");
 		dataSource.setUsername("root");
-		dataSource.setPassword("");
+		dataSource.setPassword("caelum");
 		return dataSource;
 	}
 
